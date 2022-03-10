4ca697a534e2ca8358031177ed1169f770af06b6
Samyak <samyak.jain@searce.com>
2022-03-10 18:36:32 +0530

    tic-tac-toe game

---
+++ practice/tic-tac-toe/player.py
@@ -0,0 +1,42 @@
+import math
+from pickle import FALSE
+import random
+
+class Player:
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-8: ')
+            # we are going to check that this is a correct value by trying to casr
+            # it to an integer, and if it's not, then we say its invalid
+            # if that spot is not available on the board, we also say its invalid
+            try:
+                val = int(square)
+                print(val)
+                if val not in game.available_moves():
+                    raise ValueError
+                #if it dont throw an error, it is a valid sqaure
+                valid_square = True
+            except ValueError:
+                print('Invalid Square. Try Again.')
+
+        return val
